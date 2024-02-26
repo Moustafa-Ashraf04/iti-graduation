a29190fcb335ba1b992b18598998de2e85b73203
Moustafa_Ashraf <moustafa.ashraf277@gmail.com>
2024-02-26 06:45:22 +0200

    header,carousel slider,cards in home

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { TopBarComponent } from '../top-bar/top-bar.component';
 import { HeaderComponent } from './../header/header.component';
+import { CommonModule } from '@angular/common';
+import { ProductCardComponent } from '../product-card/product-card.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ HeaderComponent,TopBarComponent,],
+  imports: [
+    HeaderComponent,
+    TopBarComponent,
+    CommonModule,
+    ProductCardComponent,
+    FooterComponent
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  items = [1, 2, 3, 4];
+
+}
